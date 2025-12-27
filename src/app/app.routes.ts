21228f51b85a55c35a57fdb3c48790a92e6bbb98
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2025-12-27 20:52:13 -0300

    updated route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
-    {
-        path: "login",
-        component: LoginComponent
-    },
-    {
-        path: "signup",
-        component: SignupComponent
-    }
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: "login",  component: LoginComponent },
+    { path: "signup",  component: SignupComponent }
 ];
